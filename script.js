8a07aad05d60c959bd766009c9f2d35f9ad88174
Gerald Lee <gerald_4231@hotmail.com>
2024-03-11 14:25:20 +0800

    dynamic bg colour change frontend

--- script.js
+++ script.js
@@ -2,12 +2,33 @@ function updatePasswordInfo() {
     const input = document.getElementById("passwordInput");
     const password = input.value;
     document.getElementById("passwordLength").textContent = "Number of Characters: " + password.length;
-
+    crackPasswordTiming()
     // Using Bootstrap's 'text-success' class
     document.getElementById("lowercase").classList.toggle("text-success", /[a-z]/.test(password));
     document.getElementById("uppercase").classList.toggle("text-success", /[A-Z]/.test(password));
     document.getElementById("numbers").classList.toggle("text-success", /[0-9]/.test(password));
     document.getElementById("symbols").classList.toggle("text-success", /[^A-Za-z0-9]/.test(password));
+}
+
+function crackPasswordTiming(){
+    num = Number(document.getElementById("years").textContent);
+    num += 1000;
+    document.getElementById("years").textContent = num;
+
+    //Change password bg
+    if(num > 2000 && num <= 5000){
+        document.getElementById("password-strength-bg").className ="bg-danger p-1 text-center rounded"
+        document.getElementById("password-strength-text").textContent = "Very Weak"
+    }
+    else if(num > 5000 && num <= 10000){
+        document.getElementById("password-strength-bg").className ="bg-warning p-1 text-center rounded"
+        document.getElementById("password-strength-text").textContent = "Medium"
+    }
+    else if(num > 10000){
+        document.getElementById("password-strength-bg").className ="bg-success p-1 text-center rounded"
+        document.getElementById("password-strength-text").textContent = "Strong"
+    }
+     
 }
 
 function togglePasswordVisibility() {
